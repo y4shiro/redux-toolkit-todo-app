4e2b80bcac6c0d8da37610b762b7b3f1ca2705b2
y4shiro <y4shironao@gmail.com>
2022-10-05 18:06:00 +0900

    Header コンポーネント内でスタイルを定義

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
 
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import styles from './index.module.scss';
 
 const index: React.FC = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position='static'>
-        <Toolbar>
-          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-            News
+    <div className={styles.root}>
+      <AppBar position='static' className={styles.app_bar}>
+        <Toolbar className={styles.tool_bar}>
+          <Typography variant='h6' component='div' className={styles.title}>
+            Redux Toolkit Todo
           </Typography>
         </Toolbar>
       </AppBar>
-    </Box>
+    </div>
   );
 };
 
