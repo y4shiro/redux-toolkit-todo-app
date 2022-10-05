dcead694b888b6d69d71508f385189c4ed190824
y4shiro <y4shironao@gmail.com>
2022-10-05 23:05:21 +0900

    TaskForm コンポーネントに React Hook Form を導入して Submit が行えるようにした

--- src/features/task/TaskForm/index.tsx
+++ src/features/task/TaskForm/index.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import TextField from '@mui/material/TextField';
 
 import styles from './index.module.scss';
 
+type InputType = {
+  taskTitle?: string;
+};
+
 const index: React.FC = () => {
+  const { register, handleSubmit, reset } = useForm();
+
+  const handleCreate = (data: InputType) => {
+    console.log(data);
+    reset();
+  };
+
   return (
     <div className={styles.root}>
-      <form className={styles.form} noValidate autoComplete='off'>
+      <form onSubmit={handleSubmit(handleCreate)} className={styles.form}>
         <TextField
+          {...register('taskTitle')}
+          name='taskTitle'
           id='outlined-basic'
           label='New Task'
           variant='outlined'
