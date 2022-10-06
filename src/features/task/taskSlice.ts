2bd1c1ca49ea275360ec1e0cef16ef7bb3b32699
y4shiro <y4shironao@gmail.com>
2022-10-06 10:56:48 +0900

    taskSlice を作成

---
+++ src/features/task/taskSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState, AppThunk } from '../../app/store';
+
+export type TaskState = {
+  idCount: number; // Task の個数
+  tasks: { id: number; title: string; completed: boolean }[];
+  selectedTask: { id: number; title: string; completed: boolean }; // 編集中の Task
+  isModalOpen: boolean; // Task を編集するモーダルの改変管理
+};
+
+const initialState: TaskState = {
+  idCount: 1,
+  tasks: [{ id: 1, title: 'TaskA', completed: false }],
+  selectedTask: { id: 0, title: '', completed: false },
+  isModalOpen: false,
+};
+
+export const taskSlice = createSlice({
+  name: 'task',
+  initialState,
+  reducers: {
+    createTask: (state, action) => {
+      state.idCount++;
+      const newTask = { id: state.idCount, title: action.payload, completed: false };
+      state.tasks = [newTask, ...state.tasks];
+    },
+  },
+});
+
+export const { createTask } = taskSlice.actions;
+export const selectTask = (state: RootState): TaskState['tasks'] => state.task.tasks;
+export default taskSlice.reducer;
